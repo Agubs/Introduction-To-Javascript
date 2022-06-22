ecbc56260583cf36c5b47035f2ca1b84bc2ab8c3
Cynthia <agubuikecynthia936@gmail.com>
2022-06-22 15:26:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var namestring = function (name) {return "Hi, I am" + " " + name + '<br/>';};
+var name = namestring ("Cynthia")
+namestring ("Cynthia"); document.write (name);
+
+var heightstring = function (height) {return "I am" + " " + height + " " + "tall" + '<br/>';};
+var height = heightstring ("179cm")
+namestring ("179cm"); document.write (height);
+
+var countrystring = function (country) {return "I am from" + " " + country + '<br/>';};
+var country = countrystring ("Nigeria")
+namestring ("Nigeria"); document.write (country);
